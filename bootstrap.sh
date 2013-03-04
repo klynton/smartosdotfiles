438f22cd6e46ec8b55e3c10a16db4c5c55e2a4e2
Klynton Jessup <klynton@objectsyndicate.com>
2013-03-04 15:35:16 -0700

    Make the bootstrap script a little less destructive.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
+
+cp $HOME/.bash_profile $HOME/.bash_profile.bak
+cp $HOME/.bashrc $HOME/.bashrc.bak
+cp $HOME/.gitconfig $HOME/.gitconfig.bak
+echo "Your original .bash_profile has been backed up to .bash_profile.bak"
+echo "Your original .bashrc has been backed up to .bashrc.bak"
+echo "Your original .gitconfig has been backed up to .gitconfig.bak"
+
 git pull
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
